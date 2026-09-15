c5242acf202762674a95292e411dd96bfa219c7a
Afmarnob <afmarnob@gmail.com>
2026-09-15 20:28:32 +0600

    added navbar

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,14 +1,4 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <p>Welcome to the Next.js app</p>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Click me
-      </button>
-      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-    </div>
-  );
+  return;
+  <></>;
 }
